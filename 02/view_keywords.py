e8d192c5b0c4826da26c56281aa4620b82330623
wangmengmeng <wangmm_0165@163.com>
2019-06-21 10:43:59 +0800

    Initial commit

---
+++ 02/view_keywords.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/21 10:40
+# @Author  : wangmengmeng
+
+import keyword
+print(keyword.kwlist)
+# output:['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+# 关键字不能作为变量
