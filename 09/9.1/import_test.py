d96dee2c0acd541f3dc17e35da69275a71e4dc31
wangmengmeng <wmm_0165@163.com>
2019-06-26 20:51:02 +0800

    模块

---
+++ 09/9.1/import_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/6/26 20:33
+# @Author : wangmengmeng
+import sys
+print(sys.argv[0])
+
+
+# D:\myproject\crazy_python\09\9.1>python import_test.py      windows下执行
+# output: import_test.py
+# sys 模块下的 argv 变量用于获取运行 Python 程序的
+# 命令行参数，其中 argv[O］用于获取该 Python 程序的程序名）
