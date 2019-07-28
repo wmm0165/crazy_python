8b2df62cfa059b1955a4003194b82ae8517ad348
wangmengmeng <wmm_0165@163.com>
2019-07-28 22:50:54 +0800

    02

---
+++ 02/2.1/comment_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/7/28 22:23
+# @Author : wangmengmeng
+# 单行注释
+print("hello world")
+'''
+三个单引号多行注释
+'''
+
+"""
+三个双引号注释也是可以的
+"""
---
+++ 02/2.4/string_test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/21 11:25
+# @Author  : wangmengmeng
+str1 = 'crazy'
+str2 = '疯狂软件教育'
+print(str1)
+print(str2)
+str4 = '"hello" world' # 字符串本身含有双引号，可使用单引号将字符串括起来
+str5 = '"we are scared, Let\'s hide in the shade", says the bird' # 字符串既有单引号又有双引号，必须使用转义符
