5dc9b690337c070585e7adce24e656cb90ac1eaa
wangmengmeng <wmm_0165@163.com>
2019-08-15 20:30:27 +0800

    6.1

---
+++ 06/6.1/Person.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/8/13 17:52
+# @Author : wangmengmeng
+class Person:
+    hair = 'black'
+    def __init__(self,name = 'Charlie',age = 8):
+        self.name = name
+        self.age = age
+
+    # def say(self):
